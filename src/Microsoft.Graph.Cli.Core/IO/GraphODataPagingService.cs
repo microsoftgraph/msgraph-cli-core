957c8d6c6e1b9b7de1a9b4115f362d42d9a3b931
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-06-06 15:46:51 +0300

    Add Graph OData paging service (#65)
    * Add Graph OData paging service

---
+++ src/Microsoft.Graph.Cli.Core/IO/GraphODataPagingService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Kiota.Cli.Commons.IO;
+
+namespace Microsoft.Graph.Cli.Core.IO;
+
+/// <summary>
+/// The graph OData paging service
+/// </summary>
+public class GraphODataPagingService : ODataPagingService
+{
+    /// <inheritdoc />
+    public override bool OnBeforeGetPagedData(PageLinkData pageLinkData, bool fetchAllPages = false)
+    {
+        // Set the page size to 999 if the user asked to fetch all pages and top either isn't specified or is invalid
+        if (fetchAllPages && (!pageLinkData.RequestInformation.QueryParameters.TryGetValue("%24top", out var topVal) || (topVal as int?) == null || (topVal as int?) < 1))
+        {
+            pageLinkData.RequestInformation.QueryParameters["%24top"] = 999;
+        }
+        return true;
+    }
+}
