cfa2ee3cbd603bab6e85a7dc71078b52b978197e
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-08-23 17:39:53 +0300

    Delete auth metadata files on logout

--- src/Microsoft.Graph.Cli.Core/Authentication/LogoutService.cs
+++ src/Microsoft.Graph.Cli.Core/Authentication/LogoutService.cs
@@ -1,15 +1,33 @@
 using System.IO;
+using Microsoft.Graph.Cli.Core.IO;
 using Microsoft.Graph.Cli.Core.Utils;
 
 namespace Microsoft.Graph.Cli.Core.Authentication;
 
 public class LogoutService
 {
+    private readonly IAuthenticationCacheUtility authCacheUtility;
+
+    private readonly IPathUtility pathUtility;
+
+    public LogoutService(IAuthenticationCacheUtility authCacheUtility, IPathUtility pathUtility)
+    {
+        this.authCacheUtility = authCacheUtility;
+        this.pathUtility = pathUtility;
+    }
+
     public void Logout()
     {
-        if (File.Exists(Constants.AuthRecordPath))
+        var authRecordPath = Path.Combine(pathUtility.GetApplicationDataDirectory(), Constants.AuthRecordPath);
+        if (File.Exists(authRecordPath))
         {
-            File.Delete(Constants.AuthRecordPath);
+            File.Delete(authRecordPath);
+        }
+
+        var authIdentifierPath = authCacheUtility.GetAuthenticationCacheFilePath();
+        if (File.Exists(authIdentifierPath))
+        {
+            File.Delete(authIdentifierPath);
         }
     }
 }
