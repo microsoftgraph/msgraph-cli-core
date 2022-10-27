1a6e0e3df58ea0103bcb382687e294aeb6895cc6
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2022-10-27 19:32:36 +0300

    Update http logging format (#113)
    * Update http logging format
    * Simplify condition for reading content as string
    * Add LoggingHandler tests

--- src/Microsoft.Graph.Cli.Core/Http/LoggingHandler.cs
+++ src/Microsoft.Graph.Cli.Core/Http/LoggingHandler.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -15,29 +17,66 @@ public class LoggingHandler : DelegatingHandler
     {
         if (Logger is null) return await base.SendAsync(request, cancellationToken);
 
-        var headers = request.Headers.Select((h) =>
-        {
-            var value = string.Join(",", h.Value);
-            if (h.Key == "Authorization")
-            {
-                value = "[PROTECTED]";
-            }
-            return string.Format("H:{0}={1}", h.Key, value);
-        }).Aggregate((a, b) => string.Join("\n\t", a, b));
-        Logger?.LogDebug("Calling:\n{0} {1}\n\t{2}", request.Method, request.RequestUri, headers);
+        Logger?.LogDebug("\nRequest:\n\n{0} {1} HTTP/{2}\n{3}\n{4}\n",
+            request.Method, request.RequestUri, request.Version,
+            HeadersToString(request.Headers, request.Content?.Headers),
+            await (request.Content?.ReadAsStringAsync() ?? Task.FromResult<string>(string.Empty))
+        );
+
         var response = await base.SendAsync(request, cancellationToken);
 
-        if (!response.IsSuccessStatusCode)
+        // If the response has a content length > 0 and is not a stream, get the content. Otherwise get the content length
+        var responseContent = string.Empty;
+        var isStream = response.Content.Headers.ContentType?.MediaType?.Contains("stream") == true;
+        if (response.Content.Headers.ContentLength > 0 && !isStream)
         {
-            Logger?.LogDebug("{0}\t{1}\t{2}", request.RequestUri,
-                (int)response.StatusCode, response.Headers.Date);
-            Logger?.LogDebug("{0}", await response.Content.ReadAsStringAsync());
+            responseContent = await response.Content.ReadAsStringAsync();
         }
+        else if (isStream)
+        {
+            string size = string.Empty;
+            if (response.Content.Headers.ContentLength > 0)
+            {
+                size = $"{response.Content.Headers.ContentLength} byte ";
+            }
+
+            responseContent = $"[...<{size}data stream>...]";
+        }
+
+        Logger?.LogDebug("\nResponse:\n\nHTTP/{0} {1} {2}\n{3}\n{4}\n",
+            response.Version, (int)response.StatusCode, response.ReasonPhrase,
+            HeadersToString(response.Headers, response.Content?.Headers),
+            responseContent
+        );
         return response;
     }
 
     protected override void Dispose(bool disposing)
     {
         base.Dispose(disposing);
+    }
+
+    private string HeadersToString(in HttpHeaders headers, in HttpHeaders? contentHeaders)
+    {
+        if (!headers.Any() && contentHeaders?.Any() == false) return string.Empty;
+        Func<KeyValuePair<string, IEnumerable<string>>, string> selector = (h) =>
+        {
+            var value = string.Join(",", h.Value);
+            if (h.Key.Contains("Authorization", StringComparison.OrdinalIgnoreCase))
+            {
+                value = "[PROTECTED]";
+            }
+            return string.Format("{0}: {1}\n", h.Key, value);
+        };
+
+        Func<string, string, string> aggregator = (a, b) => string.Join(string.Empty, a, b);
+
+        var h = headers.Select(selector).Aggregate("", aggregator);
+        if (contentHeaders != null)
+        {
+            h += contentHeaders.Select(selector).Aggregate("", aggregator);
+        }
+
+        return h;
     }
 }
