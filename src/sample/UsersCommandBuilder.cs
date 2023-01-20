22760c39662d3160c6dd75b9e814e70c0d0acced
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-01-20 15:30:50 +0300

    Update package dependencies. Update package sample

--- src/sample/UsersCommandBuilder.cs
+++ src/sample/UsersCommandBuilder.cs
@@ -2,6 +2,7 @@ using Microsoft.Kiota.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.IO;
 using System.Net.Http;
 
 namespace Microsoft.Graph.Cli
@@ -11,58 +12,51 @@ namespace Microsoft.Graph.Cli
         internal static Command BuildUsersCommand(IRequestAdapter requestAdapter)
         {
             var command = new Command("users");
-            command.Description = "Retrieve a list of user objects. This operation returns by default only a subset of the more commonly used properties for each user. These _default_ properties are noted in the Properties section. To get properties that are _not_ returned by default, do a GET operation for the user and specify the properties in a `$select` OData query option.";
+            command.Description = "Retrieve a list of user objects.";
             // Create options for all the parameters
-            var consistencyLevelOption = new Option<string>("--consistency-level", description: "Indicates the requested consistency level. Documentation URL: https://docs.microsoft.com/graph/aad-advanced-queries")
-            {
+            var consistencyLevelOption = new Option<string[]>("--consistency-level", description: "Indicates the requested consistency level. Documentation URL: https://docs.microsoft.com/graph/aad-advanced-queries") {
+                Arity = ArgumentArity.ZeroOrMore
             };
             consistencyLevelOption.IsRequired = false;
             command.AddOption(consistencyLevelOption);
-            var topOption = new Option<int?>("--top", description: "Show only the first n items")
-            {
+            var topOption = new Option<int?>("--top", description: "Show only the first n items") {
             };
             topOption.IsRequired = false;
             command.AddOption(topOption);
-            var skipOption = new Option<int?>("--skip", description: "Skip the first n items")
-            {
+            var skipOption = new Option<int?>("--skip", description: "Skip the first n items") {
             };
             skipOption.IsRequired = false;
             command.AddOption(skipOption);
-            var searchOption = new Option<string>("--search", description: "Search items by search phrases")
-            {
+            var searchOption = new Option<string>("--search", description: "Search items by search phrases") {
             };
             searchOption.IsRequired = false;
             command.AddOption(searchOption);
-            var filterOption = new Option<string>("--filter", description: "Filter items by property values")
-            {
+            var filterOption = new Option<string>("--filter", description: "Filter items by property values") {
             };
             filterOption.IsRequired = false;
             command.AddOption(filterOption);
-            var countOption = new Option<bool?>("--count", description: "Include count of items")
-            {
+            var countOption = new Option<bool?>("--count", description: "Include count of items") {
             };
             countOption.IsRequired = false;
             command.AddOption(countOption);
-            var orderbyOption = new Option<string[]>("--orderby", description: "Order items by property values")
-            {
+            var orderbyOption = new Option<string[]>("--orderby", description: "Order items by property values") {
                 Arity = ArgumentArity.ZeroOrMore
             };
             orderbyOption.IsRequired = false;
             command.AddOption(orderbyOption);
-            var selectOption = new Option<string[]>("--select", description: "Select properties to be returned")
-            {
+            var selectOption = new Option<string[]>("--select", description: "Select properties to be returned") {
                 Arity = ArgumentArity.ZeroOrMore
             };
             selectOption.IsRequired = false;
             command.AddOption(selectOption);
-            var expandOption = new Option<string[]>("--expand", description: "Expand related entities")
-            {
+            var expandOption = new Option<string[]>("--expand", description: "Expand related entities") {
                 Arity = ArgumentArity.ZeroOrMore
             };
             expandOption.IsRequired = false;
             command.AddOption(expandOption);
-            command.SetHandler(async (invocationContext) =>
-            {
+            var queryOption = new Option<string>("--query");
+            command.AddOption(queryOption);
+            command.SetHandler(async (invocationContext) => {
                 var consistencyLevel = invocationContext.ParseResult.GetValueForOption(consistencyLevelOption);
                 var top = invocationContext.ParseResult.GetValueForOption(topOption);
                 var skip = invocationContext.ParseResult.GetValueForOption(skipOption);
@@ -72,6 +66,7 @@ namespace Microsoft.Graph.Cli
                 var orderby = invocationContext.ParseResult.GetValueForOption(orderbyOption);
                 var select = invocationContext.ParseResult.GetValueForOption(selectOption);
                 var expand = invocationContext.ParseResult.GetValueForOption(expandOption);
+                var query = invocationContext.ParseResult.GetValueForOption(queryOption);
                 var cancellationToken = invocationContext.GetCancellationToken();
                 var requestInfo = new RequestInformation
                 {
@@ -81,12 +76,11 @@ namespace Microsoft.Graph.Cli
                 };
                 requestInfo.Headers.Add("Accept", "application/json");
                 requestInfo.Headers["ConsistencyLevel"] = consistencyLevel;
-                var handler = new NativeResponseHandler();
-                await requestAdapter.SendNoContentAsync(requestInfo, responseHandler: handler);
-                var response = handler.Value as HttpResponseMessage;
+                var response = await requestAdapter.SendPrimitiveAsync<Stream>(requestInfo);
                 if (response != null)
                 {
-                    Console.WriteLine(await response.Content.ReadAsStringAsync(cancellationToken));
+                    var reader = new StreamReader(response);
+                    Console.WriteLine(await reader.ReadToEndAsync());
                 }
             });
             return command;
