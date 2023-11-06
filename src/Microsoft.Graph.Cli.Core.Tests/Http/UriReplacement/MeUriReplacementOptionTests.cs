6fffc187024bd87da46105bfe7bc5255b21bfdbe
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-11-06 16:36:13 +0300

    Replace `/users/me` with `/me` in middleware. (#285)
    * Replace `/users/me` with `/me` in middleware.
    
    * Update args replacement
    
    * Refactor `UriReplacementHandler` to use generics, thus eliminating `MeUriReplacement` boxing allocations.
    
    Cleanup code.
    
    * Use from end indexing shorthand
    
    * Use explicit help detection
    
    * Move UriReplacement to kiota cli commons
    
    * Remove redundant pipeline
    
    * Revert: Remove redundant pipeline
    
    * Simplify stackalloc initialization
    
    * Update src/Microsoft.Graph.Cli.Core.Tests/Http/UriReplacement/MeUriReplacementTests.cs
    
    Co-authored-by: Peter Ombwa <peter.ombwa@microsoft.com>
    
    * Update src/Microsoft.Graph.Cli.Core.Tests/Http/UriReplacement/MeUriReplacementTests.cs
    
    Co-authored-by: Peter Ombwa <peter.ombwa@microsoft.com>
    
    * Update MeUriReplacementOption
    
    * Update MeUriReplacement to use kiota http handler
    
    * Update sonarcloud
    
    * Update .github/workflows/sonarcloud.yml
    
    Co-authored-by: Peter Ombwa <peter.ombwa@microsoft.com>
    
    * Update sonar org.
    
    * Update sonarcloud org name
    
    * Fix code smell
    
    * Add coverlet msbuild for coverage reporting
    
    * Exclude sample project from sonarcloud exclusions.
    
    * Add a me alias to users to show in help
    
    ---------
    
    Co-authored-by: Peter Ombwa <peter.ombwa@microsoft.com>

---
+++ src/Microsoft.Graph.Cli.Core.Tests/Http/UriReplacement/MeUriReplacementOptionTests.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.Graph.Cli.Core.Http.UriReplacement;
+using Xunit;
+
+namespace Microsoft.Graph.Cli.Core.Tests.Http.UriReplacement;
+
+public class MeUriReplacementOptionTests
+{
+    [Fact]
+    public void Returns_Null_When_Given_A_Null_Url()
+    {
+        var replacement = new MeUriReplacementOption();
+
+        Assert.Null(replacement.Replace(null));
+    }
+
+    [Theory]
+    [InlineData("http://example.com/test")]
+    [InlineData("http://example.com/users/messages")]
+    [InlineData("http://example.com/v1.0/users/messages")]
+    [InlineData("http://example.com/users/test/me")]
+    [InlineData("http://example.com/a/b/users/test/me")]
+    public void Returns_Original_Uri_When_No_Match_Is_Found(string inputUri)
+    {
+        var uri = new Uri(inputUri);
+        var replacement = new MeUriReplacementOption();
+
+        Assert.Equal(uri, replacement.Replace(uri));
+    }
+
+    [Theory]
+    [InlineData("http://example.com/v1.0/users/me/messages", "http://example.com/v1.0/me/messages")]
+    [InlineData("http://example.com/v1.0/users/me", "http://example.com/v1.0/me")]
+    [InlineData("http://example.com/v1.0/users/me?a=b", "http://example.com/v1.0/me?a=b")]
+    public void Returns_A_New_Url_When_A_Match_Is_Found(string inputUri, string expectedUri)
+    {
+        var replacement = new MeUriReplacementOption();
+
+        var uri = new Uri(inputUri);
+        Assert.Equal(expectedUri, replacement.Replace(uri)!.ToString());
+    }
+}
---
+++ src/Microsoft.Graph.Cli.Core/Http/UriReplacement/MeUriReplacementOption.cs
@@ -0,0 +1,86 @@
+using System;
+using Microsoft.Kiota.Http.HttpClientLibrary.Middleware.Options;
+
+namespace Microsoft.Graph.Cli.Core.Http.UriReplacement;
+
+/// <summary>
+/// Specialized replacement for /[version]/users/me with /[version]/me
+/// </summary>
+public class MeUriReplacementOption : IUriReplacementHandlerOption
+{
+    private readonly bool isEnabled;
+
+    /// <summary>
+    /// Create new MeUriReplacementOption
+    /// </summary>
+    /// <param name="isEnabled">Whether the uri replacement is enabled.</param>
+    public MeUriReplacementOption(bool isEnabled = true)
+    {
+        this.isEnabled = isEnabled;
+    }
+
+    /// <inheritdoc/>
+    public bool IsEnabled()
+    {
+        return isEnabled;
+    }
+
+    /// <summary>
+    /// If a URI path starts with /[version]/users/me, replace it with /[version]/me
+    /// </summary>
+    /// <param name="original">The original URI</param>
+    /// <returns>A URI with /[version]/users/me replaced with /[version]/me</returns>
+    /// <remarks>This method assumes that the first segment after the root is a version segment to match Microsoft Graph API's format.</remarks>
+    public Uri? Replace(Uri? original)
+    {
+        if (original is null)
+        {
+            return null;
+        }
+
+        if (!isEnabled || original.Segments.Length < 4)
+        {
+            // Must have at least segments "/", "[version]/", "users/", "me"
+            return original;
+        }
+
+        Span<char> toMatch = stackalloc char[] { '/', 'u', 's', 'e', 'r', 's', '/', 'm', 'e' };
+        var separator = toMatch[..1];
+        var matchUsers = toMatch[1..6];
+        var matchMe = toMatch[7..];
+
+        var maybeUsersSegment = original.Segments[2].AsSpan();
+        if (!maybeUsersSegment[..^1].SequenceEqual(matchUsers))
+        {
+            return original;
+        }
+
+        var maybeMeSegment = original.Segments[3].AsSpan();
+        if (!maybeMeSegment[..(maybeMeSegment.EndsWith(separator) ? ^1 : ^0)].SequenceEqual(matchMe))
+        {
+            return original;
+        }
+
+        var newUrl = new UriBuilder(original);
+        var versionSegment = original.Segments[1].AsSpan();
+        const int usersMeLength = 9;
+        var length = versionSegment.Length + usersMeLength;
+        if (newUrl.Path.Length == length)
+        {
+            // Matched /[version]/users/me
+            newUrl.Path = string.Concat(separator, versionSegment, matchMe);
+        }
+        else
+        {
+            // Maybe matched /[version]/users/me...
+            // Logic to make sure we don't match paths like /users/messages
+            var span = newUrl.Path.AsSpan(length);
+            if (span[0] == '/')
+            {
+                newUrl.Path = string.Concat(separator, versionSegment, matchMe, span);
+            }
+        }
+
+        return newUrl.Uri;
+    }
+}
